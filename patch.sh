f03a5b7348ecd500494ce623d7e475296d46935a
Brad Beckwith <brad.beckwith@gmail.com>
2023-09-28 08:58:46 -0700

    Update patch.sh
    Updated to put log files in /var/log/ and not the users home directory.

--- patch.sh
+++ patch.sh
@@ -1,7 +1,9 @@
 !#/bin/bash
-# Written by: Brad Beckwith @ home
+# Written by: Brad Beckwith
+# Updated on: 9/28/2023 USA
+echo "This is for Debian Linux types (i.e. Ubuntu, Pop-OS, etc.)
 
 apt-get update
 apt-get upgrade -y
 #apt-get dist-upgrade -y
-echo "Patched system on $(date)" >> /home/bwbeckwi/Documents/PatchedSystem.log
+echo "Patched system on $(date)" >> /var/log/PatchedSystem.log
